0f4f2feae4579ff1fe05982ec48d2359059d9687
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2022-01-07 00:19:18 -0800

    feat: change weather-icon by rangeID from data

--- src/App.js
+++ src/App.js
@@ -22,12 +22,40 @@ class App extends React.Component {
       error: false
     };
     this.getWeather();
+
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog"
+    }
   }
 
   // Celsius calculator
   calCelsius(temp){
     let cell = Math.floor(temp - 273.15);
     return cell;
+  }
+
+  get_weatherIcon(icons, rangeID){
+    if(rangeID >= 200 && rangeID <= 232){
+      this.setState({icon:this.weatherIcon.Thunderstorm})
+    } else if(rangeID >= 300 && rangeID <= 321){
+      this.setState({icon:this.weatherIcon.Drizzle})
+    } else if(rangeID >= 500 && rangeID <= 531){
+      this.setState({icon:this.weatherIcon.Rain})
+    } else if(rangeID >= 600 && rangeID <= 622){
+      this.setState({icon:this.weatherIcon.Snow})
+    } else if(rangeID >= 701 && rangeID <= 781){
+      this.setState({icon:this.weatherIcon.Atmosphere})
+    } else if(rangeID === 800){
+      this.setState({icon:this.weatherIcon.Clear})
+    } else if(rangeID >= 801 && rangeID <= 804){
+      this.setState({icon:this.weatherIcon.Clouds})
+    }
   }
 
   getWeather = async() => {
@@ -44,8 +72,11 @@ class App extends React.Component {
       celsius: this.calCelsius(response.main.temp),
       temp_max:this.calCelsius(response.main.temp_max),
       temp_min: this.calCelsius(response.main.temp_min),
-      description: response.weather[0].description
-    })
+      description: response.weather[0].description,
+    });
+
+    this.get_weatherIcon(this.weatherIcon, response.weather[0].id)
+
   }; 
 
   render(){
@@ -57,11 +88,13 @@ class App extends React.Component {
                temp_max={this.state.temp_max}
                temp_min={this.state.temp_min}
                description={this.state.description}
+               weatherIcon={this.state.icon}
       />
     </div>
   );
  }
 }
+
 
 
 
