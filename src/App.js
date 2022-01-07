f762c7b91fe19efc284e1baee77c378eee4d1fb3
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2022-01-06 17:18:39 -0800

    feat: call weather API & add data to state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'weather-icons/css/weather-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from './component/weather.component';
+import { Component } from 'react/cjs/react.production.min';
 
+const API_key = "a547490d7e1a6cc61d752508bf272726";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <Weather/>
-      </header>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined
+    };
+    this.getWeather();
+  }
+
+  getWeather = async() => {
+    const apiCall = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`
+    );
+
+    const response = await apiCall.json();
+    console.log(response);
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country 
+    })
+  }; 
+
+  render(){
+    return( 
+    <div className="App">   
+      <Weather city={this.state.city} country={this.state.country}/>
     </div>
   );
+ }
 }
+
+
+
 
 export default App;
